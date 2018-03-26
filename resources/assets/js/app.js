6f915b2882868a4db7ea1af92bd2ae6749a55e1e
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-03-26 11:23:21 -0300

    Vuex's configuration

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,11 +2,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import router from './routes/routes'
+import store from './vuex/store'
 
 // GLOBAL COMPONENTS
 Vue.component("test-component", require("./components/TestComponent.vue"));
 
 const app = new Vue({
     router,
+    store,
     el: '#app'
 });
